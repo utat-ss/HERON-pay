43320d6a492cd3a537221cde0d6b690c2496d8cf
Ali Haydaroglu <ali.haydaroglu@mail.utoronto.ca>
2017-09-09 19:06:00 -0400

    Send HK Data

--- src/can_test.c
+++ src/can_test.c
@@ -2,37 +2,40 @@
 #include "uart.h"
 #include "log.h"
 
-int main(void){	
+int main(void){
 
 	init_uart();
 
 
 	can_init(0);
-	
+
 
 	// Transmiting Code
 
-	// uint8_t data[2];
-	
+	 uint8_t data[2];
+
  //    while(1){
-	
+
 	// 	data[0] = 0x2A;
 	// 	data[1] = 0x2A;
 
 	// 	can_send_message(data, 2, 0x08);  // can use any id
 
-		
+
  //    }
 
     // Recieving code for other microcontroller
-    uint8_t data[8];
-	init_rx_mob(&rx_mob, data, 2, 0x08);
-	init_rx_interrupts(rx_mob);
+  //   uint8_t data[8];
+	// init_rx_mob(&rx_mob, data, 2, 0x08);
+	// init_rx_interrupts(rx_mob);
 
 	while(1){
+
+			data[0] = 0x2A;
+			data[1] = 0x2A;
+
+			can_send_message(data, 2, (PAY_TX | OBC_RX | HK_DATA))
+
 	}
 
 }
-
-
-
