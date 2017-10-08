f07b2b82947b0062a8b8d94c5bde146d7aff006e
shimismith <smith.shimi@gmail.com>
2017-10-07 21:36:19 -0400

    Cleaned up can_test

--- src/can_test.c
+++ src/can_test.c
@@ -4,38 +4,30 @@
 
 int main(void){
 
-	init_uart();
+	init_uart();  // tends to be useful for debugging
 
 
-	can_init(0);
+	can_init(0);  // intializes can
 
+
+	 uint8_t data[2];  // I'm just using an array of size 2 for testing make it whatever you want aslong as it's <= 8
 
 	// Transmiting Code
+    while(1){
 
-	 uint8_t data[2];
+    	// load in some data
+		data[0] = 0x2A;
+		data[1] = 0x2A;
 
- //    while(1){
-
-	// 	data[0] = 0x2A;
-	// 	data[1] = 0x2A;
-
-	// 	can_send_message(data, 2, 0x08);  // can use any id
+		can_send_message(data, 2, 0x08);  // can use any id. 
+		//We will be using ids like this in the real code - (PAY_TX | OBC_RX | HK_DATA)
 
 
- //    }
+    }
 
     // Recieving code for other microcontroller
-  //   uint8_t data[8];
-	// init_rx_mob(&rx_mob, data, 2, 0x08);
-	// init_rx_interrupts(rx_mob);
-
-	while(1){
-
-			data[0] = 0x2A;
-			data[1] = 0x2A;
-
-			can_send_message(data, 2, (PAY_TX | OBC_RX | HK_DATA))
-
-	}
+    uint8_t data[8];  // set up an array to recieve data
+	init_rx_mob(&rx_mob, data, 2, 0x08);  // initialize an rx mob
+	init_rx_interrupts(rx_mob);  // initializes interrupts
 
 }
