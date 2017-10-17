0e333373ba4c5184700fe20f1c65b6875828613c
shimismith <smith.shimi@gmail.com>
2017-10-17 19:13:42 -0400

    Much better can_test. Thank you sid.

--- src/can_test.c
+++ src/can_test.c
@@ -1,33 +1,36 @@
-#include "can.h"
-#include "uart.h"
-#include "log.h"
+#include <can/can.h>
+#include <uart/uart.h>
+#include <uart/log.h>
+#include "can_handler.h"
+
+#define IS_SENDING 0
+#define IS_MATCHING_ADDR 1
 
 int main(void){
-
 	init_uart();  // tends to be useful for debugging
-
-
 	can_init(0);  // intializes can
+	uint8_t data[2];  // I'm just using an array of size 2 for testing make it whatever you want aslong as it's <= 8
+	
+	if(IS_SENDING){
+		while(1){
+	    	// load in some data
+			data[0] = 0x2A;
+			data[1] = 0x2A;
+			can_send_message(data, 2, 0x7a);  // can use any id. 
+			//We will be using ids like this in the real code - (PAY_TX | OBC_RX | HK_DATA)
+    	}
+	} else {
+	    set_can_handler(&pay_can_handler);
+	    
+	    if(IS_MATCHING_ADDR) {
+			init_rx_mob(&rx_mob, data, 2, 0x7a);  // initialize an rx mob
+	    } else {
+			init_rx_mob(&rx_mob, data, 2, 0x70);
+	    }
 
-
-	 uint8_t data[2];  // I'm just using an array of size 2 for testing make it whatever you want aslong as it's <= 8
-
-	// Transmiting Code
-    while(1){
-
-    	// load in some data
-		data[0] = 0x2A;
-		data[1] = 0x2A;
-
-		can_send_message(data, 2, 0x08);  // can use any id. 
-		//We will be using ids like this in the real code - (PAY_TX | OBC_RX | HK_DATA)
-
-
-    }
-
-    // Recieving code for other microcontroller
-    uint8_t data[8];  // set up an array to recieve data
-	init_rx_mob(&rx_mob, data, 2, 0x08);  // initialize an rx mob
-	init_rx_interrupts(rx_mob);  // initializes interrupts
-
+		init_rx_interrupts(rx_mob);  // initializes interrupts
+		
+		print("Hello team!\n");
+		while(1){}
+	}
 }
