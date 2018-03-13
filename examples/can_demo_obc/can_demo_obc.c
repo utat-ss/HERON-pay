23f6bdd221947b3199762827ed743b72863238c2
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-03-13 19:37:49 -0400

    Print recognized packets and fields for OBC-PAY CAN example

--- examples/can_demo_obc/can_demo_obc.c
+++ examples/can_demo_obc/can_demo_obc.c
@@ -5,7 +5,10 @@
 #include <uart/uart.h>
 #include <uart/log.h>
 #include <can/can.h>
+#include <can/packets.h>
 #include <util/delay.h>
+
+int req_num = 0;    // 0-4
 
 void tx_callback(uint8_t*, uint8_t*);
 
@@ -25,11 +28,49 @@ void print_bytes(uint8_t *data, uint8_t len) {
 }
 
 void tx_callback(uint8_t* data, uint8_t* len) {
-    *len = 8;
-    char str[] = "Working";
+    // *len = 8;
+    // char str[] = "Working";
+    //
+    // for(uint8_t i = 0; i < *len; i++) {
+    //     data[i] = str[i];
+    // }
 
-    for(uint8_t i = 0; i < *len; i++) {
-        data[i] = str[i];
+    *len = 8;
+
+    switch (req_num) {
+        case 0:
+            data[0] = PAY_HK_REQ;
+            data[1] = PAY_TEMP_1;
+            break;
+
+        case 1:
+            data[0] = PAY_HK_REQ;
+            data[1] = PAY_PRES_1;
+            break;
+
+        case 2:
+            data[0] = PAY_HK_REQ;
+            data[1] = PAY_HUMID_1;
+            break;
+
+        case 3:
+            data[0] = PAY_SCI_REQ;
+            data[1] = 0b0;
+            break;
+
+        case 4:
+            data[0] = PAY_SCI_REQ;
+            data[1] = 0b1;
+            break;
+
+        default:
+            req_num = 0;
+            break;
+    }
+
+    req_num++;
+    if (req_num > 4) {
+        req_num = 0;
     }
 
     print("Transmitting Data:\n");
@@ -46,7 +87,7 @@ int main(void) {
     while (1) {
         resume_mob(&tx_mob);
         while (!is_paused(&tx_mob)) {};
-        _delay_ms(5000);
+        _delay_ms(2000);
 
         print("Status: %#02x\n", mob_status(&tx_mob));
         print("Tx error count: %d\n", CANTEC);
--- examples/can_demo_pay/can_demo_pay.c
+++ examples/can_demo_pay/can_demo_pay.c
@@ -5,6 +5,7 @@
 #include <uart/uart.h>
 #include <uart/log.h>
 #include <can/can.h>
+#include <can/packets.h>
 
 void rx_callback(uint8_t*, uint8_t);
 
@@ -25,12 +26,53 @@ void print_bytes(uint8_t *data, uint8_t len) {
     print("\n");
 }
 
+void rx_callback_hk(uint8_t* data, uint8_t len) {
+    switch (data[1]) {
+        case PAY_TEMP_1:
+            print("PAY_TEMP_1\n");
+            break;
+
+        case PAY_PRES_1:
+            print("PAY_PRES_1\n");
+            break;
+
+        case PAY_HUMID_1:
+            print("PAY_HUMID_1\n");
+            break;
+    }
+}
+
+void rx_callback_sci(uint8_t* data, uint8_t len) {
+    // Last 6 bits
+    switch (data[1] & 0b111111) {
+        case 0:
+            print("Optical 0\n");
+            break;
+
+        case 1:
+            print("Optical 1\n");
+            break;
+    }
+}
+
 void rx_callback(uint8_t* data, uint8_t len) {
-    print("RX received!\n");
-    print("%s\n", (char *) data);
+    print("\nRX received!\n");
+    // print("%s\n", (char *) data);
 
     print("Received Data:\n");
     print_bytes(data, len);
+
+    switch (data[0]) {
+        case PAY_HK_REQ:
+            print("PAY_HK_REQ\n");
+            rx_callback_hk(data, len);
+            break;
+
+        case PAY_SCI_REQ:
+            print("PAY_SCI_REQ\n");
+            rx_callback_sci(data, len);
+            break;
+    }
 }
 
 int main(void) {
