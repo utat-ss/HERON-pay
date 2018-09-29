17b7a31dfa43370f890c83b35e35f0eafc8a709a
Brytni Richards <brytniri@gmail.com>
2018-09-29 13:06:05 -0400

    Modified functions and files to match new dac program and added dac structs

--- src/heaters.c
+++ src/heaters.c
@@ -1,17 +1,34 @@
 #include "heaters.h"
 
+pin_info_t dac_cs = {
+    .pin = DAC_CS_PIN_PAY,
+    .ddr = &DAC_CS_DDR_PAY,
+    .port = &DAC_CS_PORT_PAY
+};
+
+pin_info_t clr = {
+    .pin = DAC_CLR_PIN_PAY,
+    .ddr = &DAC_CLR_DDR_PAY,
+    .port = &DAC_CLR_PORT_PAY
+};
+
+dac_t dac = {
+    .cs = &dac_cs,
+    .clr = &clr
+};
+
 // init is covered by dac_init()
 
 void heaters_set_temp_a(double temp) {
     double resistance = thermis_temp_to_resistance(temp);
     double voltage = thermis_resistance_to_voltage(resistance);
-    dac_set_voltage_a(voltage);
+    dac_set_voltage(&dac, voltage, DAC_A);
 }
 
 void heaters_set_temp_b(double temp) {
     double resistance = thermis_temp_to_resistance(temp);
     double voltage = thermis_resistance_to_voltage(resistance);
-    dac_set_voltage_b(voltage);
+    dac_set_voltage(&dac, voltage, DAC_B);
 }
 
 void heaters_set_temp_both(double temp) {
--- src/heaters.h
+++ src/heaters.h
@@ -3,6 +3,11 @@
 
 #include "thermistors.h"
 #include <dac/dac.h>
+#include <dac/pay.h>
+
+extern dac_t dac;
+extern pin_info_t dac_cs;
+extern pin_info_t clr;
 
 void heaters_set_temp_a(double temp);
 void heaters_set_temp_b(double temp);
