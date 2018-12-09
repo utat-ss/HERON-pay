7da9431e461a9ad2cacee4e82df68dd4ab28f3ae
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2018-12-08 22:35:33 -0500

    Fixed compilation issues in manual tests, renamed can_commands_test

--- manual_tests/can_commands_test/can_commands_test.c
+++ manual_tests/can_commands_test/can_commands_test.c
@@ -1,51 +1,9 @@
-#ifndef F_CPU
-#define F_CPU 8000000UL
-#endif
-
-#include <uart/uart.h>
-#include <uart/log.h>
 #include <can/can.h>
-#include <can/can_ids.h>
-#include <can/packets.h>
-#include <util/delay.h>
-#include "../../src/sensors.h"
+#include <can/data_protocol.h>
+#include <can/ids.h>
+#include <uart/uart.h>
 
-// Counter for which request to send (0-3)
-uint8_t req_num = 0;            // current
-const uint8_t NUM_REQ_NUMS = 2; // total number (count)
-
-void tx_callback(uint8_t*, uint8_t*);
-void rx_callback(const uint8_t*, uint8_t);
-
-mob_t tx_mob = {
-    .mob_num = 0,
-    .mob_type = TX_MOB,
-    .id_tag = OBC_PAY_CMD_TX_MOB_ID,
-    .ctrl = default_tx_ctrl,
-
-    .tx_data_cb = tx_callback
-};
-
-mob_t rx_mob = {
-    .mob_num = 3,
-    .mob_type = RX_MOB,
-    .dlc = 8,
-    .id_tag = OBC_DATA_RX_MOB_ID,
-    .id_mask = CAN_RX_MASK_ID,
-    // .id_mask = { 0x0000 },
-    .ctrl = default_rx_ctrl,
-
-    .rx_cb = rx_callback
-};
-
-
-void print_bytes(uint8_t *data, uint8_t len) {
-    for (uint8_t i = 0; i < len; i++) {
-        print("0x%02x ", data[i]);
-    }
-    print("\n");
-}
-
+#include "../../src/env_sensors.h"
 
 
 
@@ -58,19 +16,8 @@ void tx_callback(uint8_t* data, uint8_t* len) {
     }
 
     switch (req_num) {
-        // case 0:
-        //     data[0] = PAY_HK_REQ;
-        //     data[1] = PAY_TEMP_1;
-        //     break;
-        //
-        // case 1:
-        //     data[0] = PAY_HK_REQ;
-        //     data[1] = PAY_HUMID_1;
-        //     break;
-
-        // case 2:
         case 0:
-            data[0] = PAY_SCI_REQ;
+            data[0] = CAN_PAY_SCI;
             data[1] = PAY_SCI_TEMD;
             break;
 
