5313489d51c94d1a163bbecc17a32f8d75652c6f
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-07-16 18:45:39 -0400

    Fix errors with motor constants

--- src/motors.h
+++ src/motors.h
@@ -12,6 +12,7 @@
 
 // Indexer mode
 // For both GPIOA (GPA) and GPIOB (GPB)
+/*
 #define PEX_M1      7
 #define PEX_M0      6
 #define PEX_EN      5
@@ -21,9 +22,9 @@
 #define STEP_PIN    PC1
 #define STEP_PORT   PORTC
 #define STEP_DDR    DDRC
+*/
 
 // Phase/enable mode
-/* 
 #define MOT_SLP         4
 #define MOT_ADECAY      2
 #define MOT_M1          7
@@ -33,7 +34,6 @@
 #define MOT_BENBL_PIN   PC1
 #define MOT_BENBL_PORT  PORTC
 #define MOT_BENBL_DDR   DDRC
-*/
 
 // Fault interrupt pins
 #define MOT_FLT_A       0   // A0
