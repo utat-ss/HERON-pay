77b4b9e52f41df39b636ec929793b627ba0944ce
Tahreem Butt <tahreem@ellefsontech.com>
2019-09-21 15:45:46 -0400

    added func that prints adc channel's values

--- src/devices.h
+++ src/devices.h
@@ -12,18 +12,31 @@
 #define ADC_CS_DDR  DDRB
 
 // ADC channels - for thermistors and distance sensors
-#define ADC_THM_1      0
-#define ADC_THM_2      1
-#define ADC_THM_3      2
-#define ADC_THM_4      3
-#define ADC_THM_5      4
-#define ADC_THM_6      5
-#define ADC_THM_7      7   // This is correct according to the schematic
-#define ADC_THM_8      6   // (one pair out of order) - TODO verify
-#define ADC_THM_9      8
-#define ADC_THM_10     9
-#define ADC_POS_PHT_1  10
-#define ADC_POS_PHT_2  11
+#define ADC1_GEN_THM            0
+//ADC1 Channel 1 is GND
+#define ADC1_BOOST6_VOLT_MON    2
+#define ADC1_MOTOR_TEMP_1       3
+#define ADC1_MOTOR_TEMP_2       4
+#define ADC1_BOOST6_TEMP        5 
+#define ADC1_TBOOST10_TEMP      7
+#define ADC1_BOOST10_VOLT_MON   6
+#define ADC1_BOOST10_CURR_MON   8
+#define ADC1_BOOST6_CURR_MON    9
+//ADC1 Channel 10 is GND
+#define ADC1_BATT_VOLT_MON      11
+
+#define ADC2_MF2_THM_6          0
+#define ADC2_MF2_THM_5          1
+#define ADC2_MF2_THM_4          2
+#define ADC2_MF2_THM_3          3
+#define ADC2_MF2_THM_2          4
+#define ADC2_MF2_THM_1          5
+#define ADC2_MF1_THM_6          7   // This is correct according to the schematic
+#define ADC2_MF1_THM_5          6   // (one pair out of order) - TODO verify
+#define ADC2_MF1_THM_4          8
+#define ADC2_MF1_THM_3          9
+#define ADC2_MF1_THM_2          10
+#define ADC2_MF1_THM_1          11
 
 
 // DAC CS pin
