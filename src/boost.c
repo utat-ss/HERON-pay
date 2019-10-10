6ddb430868a747817709f46b246fb7d632858fd1
Lorna Lan <lornalan0831@gmail.com>
2019-10-10 18:31:02 -0400

    created boost converter scripts

---
+++ src/boost.c
@@ -0,0 +1,5 @@
+/*
+Functions called for enabling two boost converters on pay-ssm v4
+*/
+
+#include "boost.h"
---
+++ src/boost.h
@@ -0,0 +1,47 @@
+#ifndef BOOST_H
+#define BOOST_H
+
+#include <stdbool.h>
+
+#include <avr/io.h>
+
+#include <pex/pex.h>
+#include <uart/uart.h>
+
+#include "devices.h"
+
+// Indexer mode
+// For both GPIOA (GPA) and GPIOB (GPB)
+/*
+#define PEX_M1      7
+#define PEX_M0      6
+#define PEX_EN      5
+#define PEX_SLP     4
+#define PEX_DIR     3
+#define PEX_DECAY   2
+#define STEP_PIN    PC1
+#define STEP_PORT   PORTC
+#define STEP_DDR    DDRC
+*/
+
+// Phase/enable mode
+#define MOT_SLP         4
+#define MOT_ADECAY      2
+#define MOT_M1          7
+#define MOT_APHASE      6
+#define MOT_BPHASE      3
+#define MOT_AENBL       5
+#define MOT_BENBL_PIN   PC1
+#define MOT_BENBL_PORT  PORTC
+#define MOT_BENBL_DDR   DDRC
+
+// Fault interrupt pins
+#define MOT_FLT_A       0   // A0
+#define MOT_FLT_B       1   // A1
+
+void init_motors(void);
+void enable_motors(void);
+void disable_motors(void);
+void actuate_motors(uint16_t period, uint16_t num_cycles, bool forward);
+
+#endif
