fecc1036eb3513193e51d4da63e74d28d6245aa0
Lorna Lan <lornalan0831@gmail.com>
2019-10-10 22:02:37 -0400

    Done boost src scripts

--- src/boost.c
+++ src/boost.c
@@ -1,5 +1,37 @@
 /*
-Functions called for enabling two boost converters on pay-ssm v4
+Functions called for enabling/disabling two boost converters on pay-ssm v4
+
+Boost converter: http://www.ti.com/lit/ds/symlink/tps61088.pdf
 */
 
 #include "boost.h"
+
+void init_boosts(void) {
+    // set 10V boost associated pins
+    set_pex_pin_dir(&pex, PEX_A, 10VBST_ENBL, OUTPUT);
+    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 0);
+
+    // set 6V boost associated pins
+    set_pex_pin_dir(&pex, PEX_B, 6VBST_ENBL, OUTPUT);
+    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 0);
+}
+
+void enable_10V_boost(void) {
+    // Enable 10V boost converter by setting corresponding pex pin HIGH
+    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 1);
+}
+
+void disable_10V_boost(void) {
+    // Disable 10V boost converter by setting corresponding pex pin HIGH
+    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 0);
+}
+
+void enable_6V_boost(void) {
+    // Enable 6V boost converter by setting corresponding pex pin HIGH
+    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 1);
+}
+
+void disable_6V_boost(void) {
+    // Disable 6V boost converter by setting corresponding pex pin HIGH
+    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 0);
+}
