7b63e6d97d72aa9cf8eb2bb118e469a64f4417d8
Lorna Lan <lornalan0831@gmail.com>
2019-10-10 22:45:05 -0400

    boost manual test done, complied; modifiedmakefile for 64m1

--- src/boost.c
+++ src/boost.c
@@ -8,30 +8,30 @@ Boost converter: http://www.ti.com/lit/ds/symlink/tps61088.pdf
 
 void init_boosts(void) {
     // set 10V boost associated pins
-    set_pex_pin_dir(&pex, PEX_A, 10VBST_ENBL, OUTPUT);
-    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 0);
+    set_pex_pin_dir(&pex, PEX_A, BST_TEN_ENBL, OUTPUT);
+    set_pex_pin(&pex, PEX_A, BST_TEN_ENBL, 0);
 
     // set 6V boost associated pins
-    set_pex_pin_dir(&pex, PEX_B, 6VBST_ENBL, OUTPUT);
-    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 0);
+    set_pex_pin_dir(&pex, PEX_B, BST_SIX_ENBL, OUTPUT);
+    set_pex_pin(&pex, PEX_B, BST_SIX_ENBL, 0);
 }
 
 void enable_10V_boost(void) {
     // Enable 10V boost converter by setting corresponding pex pin HIGH
-    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 1);
+    set_pex_pin(&pex, PEX_A, BST_TEN_ENBL, 1);
 }
 
 void disable_10V_boost(void) {
     // Disable 10V boost converter by setting corresponding pex pin HIGH
-    set_pex_pin(&pex, PEX_A, 10VBST_ENBL, 0);
+    set_pex_pin(&pex, PEX_A, BST_TEN_ENBL, 0);
 }
 
 void enable_6V_boost(void) {
     // Enable 6V boost converter by setting corresponding pex pin HIGH
-    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 1);
+    set_pex_pin(&pex, PEX_B, BST_SIX_ENBL, 1);
 }
 
 void disable_6V_boost(void) {
     // Disable 6V boost converter by setting corresponding pex pin HIGH
-    set_pex_pin(&pex, PEX_B, 6VBST_ENBL, 0);
+    set_pex_pin(&pex, PEX_B, BST_SIX_ENBL, 0);
 }
