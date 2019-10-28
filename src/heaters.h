ae30f2ff32b3d128a8fa1a8656624f501725b321
LornaLan <lornalan0831@gmail.com>
2019-10-27 23:44:50 -0400

    Heaters control src

--- src/heaters.h
+++ src/heaters.h
@@ -1,12 +1,19 @@
 #ifndef HEATERS_H
 #define HEATERS_H
 
-#include <dac/dac.h>
 #include <uptime/uptime.h>
 
 #include "devices.h"
 =======
 #include <avr/eeprom.h>
 >>>>>>> devices re-mapping/overhaul
+
+// Heaters all ON/OFF only, all on PEX1
+//GPIO0 Bank B
+#define HEATER_EN_N       0
+
+void init_heaters(void);
+void heater_all_on(void);
+void heater_all_off(void);
 
 #endif
