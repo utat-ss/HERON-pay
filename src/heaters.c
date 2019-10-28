7b671ece9522f8c3b6408b08b18102ab724a80ef
LornaLan <lornalan0831@gmail.com>
2019-10-27 23:44:50 -0400

    Heaters control src

--- src/heaters.c
+++ src/heaters.c
@@ -1,12 +1,26 @@
-/*
-Comparator functionality for heaters. Requires thermistors and heaters/ammeter
-attached to payload board
+ /*
+For pay-ssm v4, all heaters are controlled with software via PEX pins
 
-Author: Brytni Richards, Bruno Almeida
+For Ganymede, all heater control pins are tied to PEX1, GPIOB0
 
-For IC COMPARATOR LO PWR SGL V SOT 23-5L Part # TS391ILT
-Digikey Link: https://www.digikey.ca/product-detail/en/stmicroelectronics/TS391ILT/497-2275-1-ND/599229
-Datasheet: https://www.st.com/content/ccc/resource/technical/document/datasheet/de/4c/b3/3d/64/7d/48/8e/CD00001660.pdf/files/CD00001660.pdf/jcr:content/translations/en.CD00001660.pdf
-*/
+For now no PWM control, just turn it all ON or all OFF all the time.
+
+To-do: PWM control (probably 1K frequency)
+
+Author: Lorna Lan
+ */
 
 #include "heaters.h"
+
+void init_heaters(void){
+  set_pex_pin_dir(&pex1, PEX_B, HEATER_EN_N, OUTPUT);
+  set_pex_pin(&pex1, PEX_B, HEATER_EN_N, 1);
+}
+
+void heater_all_on(void) {
+    set_pex_pin(&pex1, PEX_B, HEATER_EN_N, 0);
+}
+
+void heater_all_off(void) {
+    set_pex_pin(&pex1, PEX_B, HEATER_EN_N, 1);
+}
--- src/heaters.h
+++ src/heaters.h
@@ -1,10 +1,17 @@
 #ifndef HEATERS_H
 #define HEATERS_H
 
-#include <dac/dac.h>
 #include <uptime/uptime.h>
 
 #include "devices.h"
 #include <avr/eeprom.h>
+
+// Heaters all ON/OFF only, all on PEX1
+//GPIO0 Bank B
+#define HEATER_EN_N       0
+
+void init_heaters(void);
+void heater_all_on(void);
+void heater_all_off(void);
 
 #endif
