84aa8bd223f55ef66d0fcc3e7e02af5e30fa9ef3
LornaLan <lornalan0831@gmail.com>
2020-01-15 23:23:36 -0500

    compiled heaters test code

--- manual_tests/heaters_test/heater_control.c
+++ manual_tests/heaters_test/heater_control.c
@@ -1,18 +1,14 @@
-#include <uptime/uptime.h>
-#include <avr/eeprom.h>
+#include <adc/adc.h>
 #include <conversions/conversions.h>
+#include <pex/pex.h>
+#include <uart/uart.h>
 
-#include "../../src/devices.h"
+#include "../../src/boost.h"
+#include "../../src/heaters.h"
 
-// Heaters all ON/OFF only (no PWM), controlled by PEX2
-// All on Bank B
-#define HEATER1_EN_N       3
-#define HEATER2_EN_N       4
-#define HEATER3_EN_N       5
-#define HEATER4_EN_N       6
-#define HEATER5_EN_N       7
+//define global variables
 
-// TODO have a function to reject invalid setpoint - in CAN
+//temperature constants, all in degree Celsius
 #define SETPOINT 33
 #define ULL -40
 #define UHL 120
@@ -22,84 +18,8 @@ uint8_t HEATERS_STATUS;
 double THERM_READINGS[12];
 uint8_t THERM_ERR_CODE[12];
 
-void init_heaters(void){
-  set_pex_pin_dir(&pex2, PEX_B, HEATER1_EN_N, OUTPUT);
-  set_pex_pin(&pex2, PEX_B, HEATER1_EN_N, 1);
-
-  set_pex_pin_dir(&pex2, PEX_B, HEATER2_EN_N, OUTPUT);
-  set_pex_pin(&pex2, PEX_B, HEATER2_EN_N, 1);
-
-  set_pex_pin_dir(&pex2, PEX_B, HEATER3_EN_N, OUTPUT);
-  set_pex_pin(&pex2, PEX_B, HEATER3_EN_N, 1);
-
-  set_pex_pin_dir(&pex2, PEX_B, HEATER4_EN_N, OUTPUT);
-  set_pex_pin(&pex2, PEX_B, HEATER4_EN_N, 1);
-
-  set_pex_pin_dir(&pex2, PEX_B, HEATER5_EN_N, OUTPUT);
-  set_pex_pin(&pex2, PEX_B, HEATER5_EN_N, 1);
-}
-
-void heater_all_on(void) {
-    set_pex_pin(&pex2, PEX_B, HEATER1_EN_N, 0);
-    set_pex_pin(&pex2, PEX_B, HEATER2_EN_N, 0);
-    set_pex_pin(&pex2, PEX_B, HEATER3_EN_N, 0);
-    set_pex_pin(&pex2, PEX_B, HEATER4_EN_N, 0);
-    set_pex_pin(&pex2, PEX_B, HEATER5_EN_N, 0);
-}
-
-void heater_all_off(void) {
-    set_pex_pin(&pex2, PEX_B, HEATER1_EN_N, 1);
-    set_pex_pin(&pex2, PEX_B, HEATER2_EN_N, 1);
-    set_pex_pin(&pex2, PEX_B, HEATER3_EN_N, 1);
-    set_pex_pin(&pex2, PEX_B, HEATER4_EN_N, 1);
-    set_pex_pin(&pex2, PEX_B, HEATER5_EN_N, 1);
-}
-
-void heater_on(uint8_t heater_num){
-    switch(heater_num){
-    case 1:
-        set_pex_pin(&pex2, PEX_B, HEATER1_EN_N, 0);
-        break;
-    case 2:
-        set_pex_pin(&pex2, PEX_B, HEATER2_EN_N, 0);
-        break;
-    case 3:
-        set_pex_pin(&pex2, PEX_B, HEATER3_EN_N, 0);
-        break;
-    case 4:
-        set_pex_pin(&pex2, PEX_B, HEATER4_EN_N, 0);
-        break;
-    case 5:
-        set_pex_pin(&pex2, PEX_B, HEATER5_EN_N, 0);
-        break;
-    default:
-        return;
-        break;
-    }
-}
-
-void heater_off(uint8_t heater_num){
-    switch(heater_num){
-    case 1:
-        set_pex_pin(&pex2, PEX_B, HEATER1_EN_N, 1);
-        break;
-    case 2:
-        set_pex_pin(&pex2, PEX_B, HEATER2_EN_N, 1);
-        break;
-    case 3:
-        set_pex_pin(&pex2, PEX_B, HEATER3_EN_N, 1);
-        break;
-    case 4:
-        set_pex_pin(&pex2, PEX_B, HEATER4_EN_N, 1);
-        break;
-    case 5:
-        set_pex_pin(&pex2, PEX_B, HEATER5_EN_N, 1);
-        break;
-    default:
-        return;
-        break;
-    }
-}
+// TODO: write a case where all thermistors are eliminated (rip) so we don't divide by zero
+// TODO: include math.h for sqrt function (and log for temperature conversion)
 
 /*
  * ABOUT THERM_ERR_CODE
@@ -115,32 +35,7 @@ void heater_off(uint8_t heater_num){
  *
  */
 
-void init_control_loop(void){
-    // 0 means elminated, 1 means normal
-    THERM_STATUS = 0x0fff;
-
-    // 0 means OFF, 1 means ON
-    HEATERS_STATUS = 0x00;
-    for (uint8_t i = 0; i < 12; i++){
-        THERM_READINGS[i] = 0.07; //some specific double, this number doesn't matter anyway
-        THERM_ERR_CODE[i] = 0;
-    }
-}
-
-// TODO: not sure if this need to be atomic when polling ADC data, will see
-void acquire_therm_data(void){
-    // poll all ADC2 channels
-    fetch_all_adc_channels(&adc2);
-
-    //convert to temperature and store in THERM_READINGS
-    for (uint8_t i = 0; i < 12; i++){
-        uint16_t raw_data = read_adc_channel(&adc2, i);
-        double therm_temp = adc_raw_to_therm_temp(raw_data);
-        THERM_READINGS[i] = therm_temp;
-    }
-}
-
-void eliminate_bad_therm(){
+void eliminate_bad_therm_sim(){
     for(uint8_t i = 0; i < 12; i++){
         //compare with both lower and upper limits first
         if(THERM_READINGS[i] < ULL){
@@ -180,128 +75,8 @@ void eliminate_bad_therm(){
     double variance = sum/(valid_therm_num - 1);*/
 }
 
-void heater_toggle(double calc_num, uint8_t heater_num){
-    //NOTE: heater_num here is the physical heater number - 1
 
-    // hot case
-    if(calc_num > SETPOINT){
-        if(HEATERS_STATUS & (0x01 << heater_num)){
-            //heater ON, need to turn it OFF
-            heater_off(heater_num+1);
-            HEATERS_STATUS = HEATERS_STATUS & ~(0x01 << heater_num);
-        }
-    }
-    // cold case
-    else if(calc_num < SETPOINT){
-        if(!(HEATERS_STATUS & (0x01 << heater_num))){
-            //heater OFF, need to turn it ON
-            heater_on(heater_num+1);
-            HEATERS_STATUS = HEATERS_STATUS | (0x01 << heater_num);
-        }
-    }
-}
-
-// fine I decided to hardcode the heaters based on physical setup
-void heater_3in_ctrl(void){
-    //looking at heater1 & 3, remember to minus one for bit shift in function argument
-    double avg_reading = 0.0;
-    double sum = 0.0;
-    uint8_t normal_therm_num = 0;
-
-    // heater 2
-    // averaging TH3-5
-    for(uint8_t i = 3; i < 6; i++){
-        if(THERM_ERR_CODE[i] == 0){
-            sum += THERM_READINGS[i];
-            normal_therm_num += 1;
-        }
-    }
-
-    avg_reading = (sum/normal_therm_num);
-    heater_toggle(avg_reading, 1);
-
-    // heater 4
-    // averaging TH9-11
-    sum = 0.0;
-    normal_therm_num = 0;
-    for(uint8_t i = 9; i < 12; i++){
-        if(THERM_ERR_CODE[i] == 0){
-            sum += THERM_READINGS[i];
-            normal_therm_num += 1;
-        }
-    }
-
-    avg_reading = (sum/normal_therm_num);
-    heater_toggle(avg_reading, 3);
-
-}
-
-void heater_4in_ctrl(void){
-    //looking at heater2 & 4, remember to minus one for bit shift in function argument
-    //
-    // if want to introduce weighted average should do it here
-    // could incorporate the weight into error code
-    double avg_reading = 0.0;
-    double sum = 0.0;
-    uint8_t normal_therm_num = 0;
-
-    // heater 1
-    // averaging TH0-2 and TH11
-    for(uint8_t i = 0; i < 3; i++){
-        if(THERM_ERR_CODE[i] == 0){
-            sum += THERM_READINGS[i];
-            normal_therm_num += 1;
-        }
-    }
-
-    if(THERM_ERR_CODE[11] == 0){
-        sum += THERM_READINGS[11];
-        normal_therm_num += 1;
-    }
-
-    avg_reading = (sum/normal_therm_num);
-    heater_toggle(avg_reading, 0);
-
-    // heater 3
-    // averaging TH5-8
-    sum = 0.0;
-    normal_therm_num = 0;
-    for(uint8_t i = 5; i < 9; i++){
-        if(THERM_ERR_CODE[i] == 0){
-            sum += THERM_READINGS[i];
-            normal_therm_num += 1;
-        }
-    }
-
-    avg_reading = (sum/normal_therm_num);
-    heater_toggle(avg_reading, 2);
-
-}
-
-void heater_5in_ctrl(void){
-    // looking at heater 5 here, average everything
-    double avg_reading = 0.0;
-    double sum = 0.0;
-    uint8_t normal_therm_num = 0;
-
-    for(uint8_t i = 0; i < 12; i++){
-        if(THERM_ERR_CODE[i] == 0){
-            sum += THERM_READINGS[i];
-            normal_therm_num += 1;
-        }
-    }
-
-    avg_reading = (sum/normal_therm_num);
-    heater_toggle(avg_reading, 4);
-}
-
-void average_heaters(void){
-    heater_3in_ctrl();
-    heater_4in_ctrl();
-    heater_5in_ctrl();
-}
-
-void heater_ctrl_status(void){
+void heater_ctrl_status_print(void){
     //print thermistors status
     for(uint8_t i = 0; i < 12; i++){
         if(THERM_ERR_CODE[i] == 0){
@@ -330,15 +105,15 @@ void heater_ctrl_status(void){
     print("\n");
 }
 
-void heater_control(void){
+void heater_control_sim(void){
     print("TH1,TH2,TH3,TH4,TH5,TH6,TH7,TH8,TH9,TH10,TH11,TH12,H1,H2,H3,H4,H5\n");
     while(1){
         acquire_therm_data();
-        eliminate_bad_therm();
+        eliminate_bad_therm_sim();
         average_heaters();
 
         // a debugging function full of print statements
-        heater_ctrl_status();
+        heater_ctrl_status_print();
 
         // current delay, every 20 seconds
         _delay_ms(20000);
