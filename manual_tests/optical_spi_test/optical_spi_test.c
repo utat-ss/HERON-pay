75a2e41e120f35da3136fc1f7ec5815a19311692
YongDaLi <yongdali314@gmail.com>
2020-01-25 01:07:01 -0500

    non-blocking receive SPI data from OPTICAL

--- manual_tests/optical_spi_test/optical_spi_test.c
+++ manual_tests/optical_spi_test/optical_spi_test.c
@@ -26,14 +26,34 @@ int main(void){
 
 
     // SPI transfer parameters
-    uint8_t well_data = 0b10000000; // bit7 optical=0, 1=LED;  bit5-0 = 0-31 well number
+    uint8_t cmd_opcode = CMD_GET_READING;
+    uint8_t well_info = _BV(7) | 0x1; // bit7 optical=0, 1=LED;  bit5-0 = 0-31 well number
+
+    // loop through all 32 wells, taking optical and LED readings
+    uint8_t well_number = 0;
+    uint8_t test_type = 0;
+
+    uint32_t received_data = 0;
 
     while (1) {
         // print("\nhello world");
         // _delay_ms(500);
 
-        uint32_t data = get_measurement(well_data);
-        print("Data in well number %d: %U\n", well_data & 0x1F, data);
+        for (well_number = 0; well_number<32; well_number++){
+            for (test_type = 0; test_type<2; test_type++){
+                // properly encode well_info
+                well_info = test_type<<7 | well_number;
+
+                // send command
+                send_opt_spi_cmd(cmd_opcode, well_info);
+
+                // wait a while before checking OPTICAL
+                _delay_ms(10);
+
+                received_data = check_received_opt_data(NUM_GET_READING);     // expecting 3 return bytes
+                print("well_number %d, test_type %d: %X", well_number, test_type, received_data);
+            }
+        }
     }
 
     return 0;
