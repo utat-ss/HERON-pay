08b51a3734735b3d212b4182e442c45ceda18acc
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2020-02-08 15:34:13 -0500

    Fix system diagnostic errors and update heartbeat implementation

--- harness_tests/sys_diagnostic/main1.c
+++ harness_tests/sys_diagnostic/main1.c
@@ -3,42 +3,48 @@
 
 #include "../../src/general.h"
 
-#define TEST_SIZE 2
-
 /* Resets the OBC SSM and verifies that the reset counter increments correctly */
 void obc_reset_test(void){
     uint32_t stale_num_restarts = 0x00;
 
-    hb_send_obc_req = true;
+    obc_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
-    stale_num_restarts = hb_latest_restart_count;
+    _delay_ms(100);
+    stale_num_restarts = obc_hb_dev.restart_count;
 
-    send_hb_reset(HB_OBC);
+    send_hb_reset((hb_dev_t*) &obc_hb_dev);
 
     _delay_ms(1000);
-    hb_send_obc_req = true;
+    obc_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
+    _delay_ms(100);
 
-    ASSERT_EQ(hb_latest_restart_count, stale_num_restarts + 1);
-    ASSERT_EQ(hb_latest_restart_reason, UPTIME_RESTART_REASON_EXTRF);
+    ASSERT_EQ(obc_hb_dev.restart_count, stale_num_restarts + 1);
+    ASSERT_EQ(obc_hb_dev.restart_reason, UPTIME_RESTART_REASON_EXTRF);
 }
 
 /* Resets the EPS SSM and verifies that the reset counter increments correctly */
 void eps_reset_test(void){
     uint32_t stale_num_restarts = 0x00;
 
-    hb_send_eps_req = true;
+    eps_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
-    stale_num_restarts = hb_latest_restart_count;
+    _delay_ms(100);
+    stale_num_restarts = eps_hb_dev.restart_count;
 
-    send_hb_reset(HB_EPS);
+    send_hb_reset((hb_dev_t*) &eps_hb_dev);
 
     _delay_ms(1000);
-    hb_send_eps_req = true;
+    eps_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
+    _delay_ms(100);
 
-    ASSERT_EQ(hb_latest_restart_count, stale_num_restarts + 1);
-    ASSERT_EQ(hb_latest_restart_reason, UPTIME_RESTART_REASON_EXTRF);
+    ASSERT_EQ(eps_hb_dev.restart_count, stale_num_restarts + 1);
+    ASSERT_EQ(eps_hb_dev.restart_reason, UPTIME_RESTART_REASON_EXTRF);
 }
 
 test_t t1 = { .name = "OBC Reset Test", .fn = obc_reset_test };
@@ -49,6 +55,6 @@ test_t* suite[] = { &t1, &t2 };
 int main(void) {
     init_pay();
     init_hb(HB_PAY);
-    run_tests(suite, TEST_SIZE);
+    run_tests(suite, sizeof(suite) / sizeof(suite[0]));
     return 0;
 }
